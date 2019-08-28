e8e32def824a396a4296a9d3245c2497a5ad0439
mojo <johannes.mogster@mojotech.no>
2019-08-28 13:02:18 +0200

    ROS node ready for hw test

--- scripts/iiwa_udp_comms.py
+++ scripts/iiwa_udp_comms.py
@@ -6,61 +6,89 @@ import socket
 import rospy
 import tf_conversions
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped, Quaternion
 
 
 UDP_IP = ''
 UDP_PORT = 30001
 TEST_COMMAND = "[X=-312.74 Y=1.00 Z=900.00 A=-0.00 B=-1.13 C=0.00]"
+CURRENT_COMMAND = ""
 SOCK = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
 
-# sock.settimeout(10)
+SOCK.settimeout(5)
 SOCK.bind((UDP_IP, UDP_PORT))
 
 
 def callback(data):
     """ function docstring, yo! """
-    print data
+    global CURRENT_COMMAND  # shame! shame! shame!
+    tmp = tf_conversions.transformations.euler_from_quaternion(
+        (
+            data.orientation.x,
+            data.orientation.y,
+            data.orientation.z,
+            data.orientation.w
+        )
+        )
+
+    C = tmp[0]
+    B = tmp[1]
+    A = tmp[2]
+
+    X = data.position.x
+    Y = data.position.y
+    Z = data.position.z
+
+    CURRENT_COMMAND = "[X=%s Y=%s Z=%s A=%s B=%s C=%s]" % (X, Y, Z, A, B, C)
 
 
 def talker_listener():
+    global CURRENT_COMMAND  # shame! shame! shame!
+    cartesian_pose_pub = rospy.Publisher(
+        'cartesian_pose',
+        PoseStamped,
+        queue_size=1
+        )
 
-    cartesian_pose_pub = rospy.Publisher('cartesian_pose', Pose, queue_size=1)
-    #rospy.init_node('kuka_cartesian_pose', anonymous=True)
     rospy.init_node('kuka_udp_comms', anonymous=True)
     rospy.Subscriber("target_EE_pose", Pose, callback)
-    rate = rospy.Rate(10)  # 10hz
+    rate = rospy.Rate(1)
 
+    while not rospy.is_shutdown():
+        try:
 
-    #while not rospy.is_shutdown():
-    try:
+            msg = PoseStamped()
+            msg.header.stamp = rospy.get_rostime()
 
-        msg = Pose()
-        # msg.header.stamp = rospy.get_rostime()
+            data, addr = SOCK.recvfrom(1024)
 
-        data, addr = SOCK.recvfrom(1024)
+            d = dict(
+                item.split('=') for item in data[1:len(data)-1].split(' ')
+                )
 
-        d = dict(
-            item.split('=') for item in data[1:len(data)-1].split(' ')
-            )
+            msg.pose.position.x = float(d["X"])
+            msg.pose.position.y = float(d["Y"])
+            msg.pose.position.z = float(d["Z"])
 
-        msg.position.x = float(d["X"])
-        msg.position.y = float(d["Y"])
-        msg.position.z = float(d["Z"])
+            msg.pose.orientation = Quaternion(
+                *tf_conversions.transformations.quaternion_from_euler(
+                    float(d['C']),
+                    float(d['B']),
+                    float(d['A'])
+                    )
+                    )
 
-        msg.orientation = tf_conversions.transformations.quaternion_from_euler(
-            float(d['A']), float(d['B']), float(d['C']),
-            'szyx'
-            )
+            cartesian_pose_pub.publish(msg)
 
-        cartesian_pose_pub.publish(msg)
+            SOCK.sendto(CURRENT_COMMAND, addr)
 
-        SOCK.sendto(TEST_COMMAND, addr)
-        SOCK.close()
-    except Exception as exception_e:
-        rospy.logerr("%s", exception_e)
+        except socket.timeout as exception_e:
+            rospy.logerr("socket timeout. check connection to iiwa.")
+        except Exception as exception_e:
+            rospy.logerr("%s", exception_e)
 
-    # rate.sleep()
+    rate.sleep()
 
 
 if __name__ == '__main__':
